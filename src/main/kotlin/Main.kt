16bf429715c87cd7494902063c33e192a990edb2
Taras Zubrei <taras.zubrei.ext@crossengage.io>
2018-12-17 19:30:15 +0200

    Calculation architecture and 2 implementations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,7 +11,14 @@ fun main(args: Array<String>) {
         val m = queryMap().get("m").integerValue() ?: 0
         val n = queryMap().get("n").integerValue() ?: 0
         val alg = queryMap().get("alg").integerValue() ?: 0
-        val algorithm = findAlgorithm(alg)(n, m)
+        val inputs = Inputs(
+                lambda = queryMap().get("lambda").integerValue() ?: 0,
+                mu = queryMap().get("mu").integerValue() ?: 0,
+                nu = queryMap().get("nu").integerValue() ?: 0
+        )
+        val algorithm = findAlgorithm(alg)(m, n)
+        val p = algorithm.calculateP(inputs)
+        val (queueSize, queueTime, workingChannels) = algorithm.calculateAverages(inputs, p)
         createHTML().html {
             head {
                 link("css/main.css", rel = "stylesheet")
@@ -21,6 +28,48 @@ fun main(args: Array<String>) {
                     if (num < n + m) printArrow(left = 11 + (80) * (num + 1) + 90 * num, top = 0, text = format(algorithm.generateLambda(num), "λ"))
                     if (num < n + m) printBackwardArrow(left = 11 + (80) * (num + 1) + 90 * num, top = 40, text = format(algorithm.generateMu(num), "µ") + format(algorithm.generateNu(num), "ν", prefix = " + "))
                     printBlock(left = 10 + (170 * num), top = 20, text = num.toString())
+                }
+                table {
+                    style = "margin-top: 150px; position: absolute; padding: 5px;"
+                    thead {
+                        tr {
+                            th {
+                                +"Середнє число заявок в черзі"
+                            }
+                            th {
+                                +"Середнє час очікування в черзі"
+                            }
+                            th {
+                                +"Середня кількість заянятих каналів"
+                            }
+                            p.mapIndexed { index, p ->
+                                th {
+                                    +"P"
+                                    sub {
+                                        +index.toString()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    tbody {
+                        tr {
+                            td {
+                                +String.format("%.2f", queueSize)
+                            }
+                            td {
+                                +String.format("%.2f", queueTime)
+                            }
+                            td {
+                                +String.format("%.2f", workingChannels)
+                            }
+                            p.map {
+                                td {
+                                    +String.format("%.2f", it)
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
--- src/main/kotlin/Processors.kt
+++ src/main/kotlin/Processors.kt
@@ -1,3 +1,19 @@
+import kotlin.math.pow
+
+data class Inputs(
+        val lambda: Int,
+        val mu: Int,
+        val nu: Int
+)
+
+data class Averages(
+        val queueSize: Double,
+        val queueTime: Double,
+        val workingChannels: Double
+)
+
+fun factorial(num: Int) = if (num == 0) 1 else (1..num).reduce(Int::times)
+
 interface Algorithm {
     val m: Int
     val n: Int
@@ -5,6 +21,8 @@ interface Algorithm {
     fun generateLambda(num: Int): Int
     fun generateMu(num: Int): Int
     fun generateNu(num: Int): Int
+    fun calculateP(data: Inputs): List<Double>
+    fun calculateAverages(data: Inputs, p: List<Double>): Averages
 }
 
 fun findAlgorithm(id: Int): (Int, Int) -> Algorithm {
@@ -16,18 +34,52 @@ fun findAlgorithm(id: Int): (Int, Int) -> Algorithm {
 }
 
 class MultichannelWithDecline(override val m: Int, override val n: Int) : Algorithm {
+    override fun calculateP(data: Inputs): List<Double> {
+        val ro = data.lambda.toDouble() / data.mu
+        val p0 = (0.rangeTo(n).map { ro.pow(it) / factorial(it) }.sum() + (ro.pow(n) / factorial(n)) * ((ro / n - (ro / n).pow(m + 1)) / (1 - (ro / n)))).pow(-1)
+        return listOf(p0).plus(
+                1.rangeTo(n).map { p0 * ro.pow(it) / factorial(it) }
+        ).plus(
+                1.rangeTo(m).map { p0 * ro.pow(n + it) / (n.toDouble().pow(it) * factorial(n)) }
+        )
+    }
+
+    override fun calculateAverages(data: Inputs, p: List<Double>): Averages {
+        val ro = data.lambda.toDouble() / data.mu
+        val workingChannels = ro * (1 - p[0] * ro.pow(n + m) / (n.toDouble().pow(m) * factorial(n)))
+        val queueSize = (p[0] * ro.pow(n + m) / (n * factorial(n))) * 1.rangeTo(m).map { it * (ro / n).pow(it - 1) }.sum()
+        return Averages(queueSize = queueSize, queueTime = queueSize / data.lambda, workingChannels = workingChannels)
+    }
+
     override val name: String
         get() = "Багатоканальна з відмовами"
 
-    override fun generateLambda(num: Int) = num + 1
+    override fun generateLambda(num: Int) = 1
 
-    override fun generateMu(num: Int) = 1
+    override fun generateMu(num: Int) = if (num < n) num + 1 else n
 
     override fun generateNu(num: Int) = 0
 }
 
 
 class MultichannelWithLimitedQueueTime(override val m: Int, override val n: Int) : Algorithm {
+    override fun calculateAverages(data: Inputs, p: List<Double>): Averages {
+        val queueSize = p.asSequence().drop(n + 1).mapIndexed { index, p -> (index + 1) * p }.sum()
+        return Averages(queueTime = data.nu.toDouble().pow(-1), queueSize = queueSize, workingChannels = (data.lambda - data.nu * queueSize) / data.mu)
+    }
+
+    override fun calculateP(data: Inputs): List<Double> {
+        val p0 = (0.rangeTo(n).map { (data.lambda.toDouble() / data.mu).pow(it) / factorial(it) }.sum() + ((data.lambda.toDouble() / data.mu).pow(n) / factorial(n)) * (1.rangeTo(m).map {
+            data.lambda.toDouble().pow(it) / (1.rangeTo(it).map { n * data.mu + it * data.nu }.reduce(Int::times))
+        }.sum())).pow(-1)
+        return listOf(p0).plus(
+                1.rangeTo(n).map { (data.lambda.toDouble() / data.mu).pow(it) / factorial(it) * p0 }
+        ).plus(
+                (1).rangeTo(m).map { (data.lambda.toDouble() / data.mu).pow(n) / factorial(n) * p0 * data.lambda.toDouble().pow(it) / (1.rangeTo(it).map { n * data.mu + it * data.nu }.reduce(Int::times)) }
+        )
+    }
+
+
     override val name: String
         get() = "Багатоканальна з обмеженим часом перебування в черзі"
 
